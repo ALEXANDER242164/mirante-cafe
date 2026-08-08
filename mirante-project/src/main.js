8251f75f5def05856d6d869e9c51d5148de2e328
Alexander <A24216477@alumnos.uady.mx>
2026-08-08 16:01:31 -0500

    Agrega sección de información y mapa en la página principal, incluyendo estilos y scripts de Leaflet para la visualización del mapa.

--- mirante-project/src/main.js
+++ mirante-project/src/main.js
@@ -63,5 +63,11 @@ async function cargarDatos(){
 
 }
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 // Se ejecuta apenas carga el script, disparando la carga de datos
 cargarDatos();
