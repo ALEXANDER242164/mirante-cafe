612ba393ce857a62fb7d66a41682e3625e9d24e7
Alexander <A24216477@alumnos.uady.mx>
2026-08-07 20:41:29 -0500

    Actualiza el HTML, CSS y JavaScript para mejorar la estructura y agregar comentarios de clientes

--- mirante-project/src/main.js
+++ mirante-project/src/main.js
@@ -7,6 +7,10 @@ async function cargarDatos(){
     // hasta que la respuesta llegue (sin bloquear el resto de la página)
     const response = await fetch('./src/data.json');
     // La respuesta llega en crudo; .json() la convierte en un objeto JS usable
+
+    if(!response.ok){
+        throw new Error('No se puede cargar data.json (HTTP ${response.status})');
+    }
     const data = await response.json();
 
     console.log(data);//Ve los datos
@@ -17,7 +21,44 @@ async function cargarDatos(){
     // Busca el <p class="intro__tag"> y le asigna el texto de la descripción
     document.querySelector('p.intro__tag').textContent = data.site.descripcion;
 
+    // Insertar comentarios
+    const comentariosContainer = document.querySelector('.list');
 
+    //borra  los comentariode prueba
+    comentariosContainer.innerHTML = '';
+
+    data.comentarios.forEach(comentario => {
+           
+
+            const li = document.createElement("li");
+            li.classList.add("card");
+
+            // crear el h3
+            const nombre = document.createElement("h3");
+            nombre.textContent = comentario.nombre;
+            
+
+            // crear el p
+            const comentarioP = document.createElement("p");
+            comentarioP.textContent = comentario.comentario;
+
+            // agregar h3 y p al li
+            li.appendChild(nombre);
+            li.appendChild(comentarioP);
+
+
+            // agregar li al contenedor
+            comentariosContainer.appendChild(li);
+
+        
+    });
+
+    //La copia
+    const track = document.querySelector('.testimonials-track');
+    const copia = comentariosContainer.cloneNode(true);
+    copia.setAttribute('aria-hidden', 'true');
+
+    track.appendChild(copia);
 
 
 }
